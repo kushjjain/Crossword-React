34641931e59f36b3d9800ce545dc6410362b71c3
kushjjain <kushjjain@gmail.com>
2024-08-27 17:22:06 +0530

    third commit

--- src/Components/CrossGrid.jsx
+++ src/Components/CrossGrid.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 
 const CrossGrid = () => {
     const grid = [
@@ -13,21 +14,44 @@ const CrossGrid = () => {
         ['O', 'T', 'C', 'O', 'N', 'T', 'I', 'N', 'U', 'E'],
         ['R', 'E', 'T', 'U', 'R', 'N', 'U', 'K', 'X', 'Y'],
       ];
+    const [selectedCells,setSelectedCells] = useState([]);
+
+    const isAdjacent = (rowIndex,colIndex) => {
+        if(selectedCells.length === 0) return true;
+
+        const [lastRow,lastCol] = selectedCells[selectedCells.length - 1] 
+        const rowDiff = Math.abs(lastRow - rowIndex)
+        const colDiff = Math.abs(lastCol - colIndex)
+        return (rowDiff ===1 && colDiff ===0) || (rowDiff === 0 && colDiff===1) || (rowDiff === 1 && colDiff=== 1);
+    }
+
+    const handleClick = (rowIndex,colIndex) => {
+        if(isAdjacent(rowIndex,colIndex)){
+            setSelectedCells([...selectedCells,[rowIndex,colIndex]]);
+        }
+        else{
+            setSelectedCells([]);
+        }
+    }
 
   return (
     <div className='grid grid-cols-10 bg-gray-100 border-4 border-violet-500 rounded-lg overflow-hidden shadow-lg' style={{width:'500px', height:'500px'}}>
         {grid.map((row,rowIndex) => (
-            row.map((col,colIndex) => (
+            row.map((col,colIndex) => {
+                const isSelected = selectedCells.some(([r, c]) => r === rowIndex && c === colIndex);
+                return(
                 <div
-                key = {`${colIndex}`}
-                className = 'flex items-center justify-center text-lg font-semibold text-gray-950 bg-white border border-gray-400 hover:bg-violet-200 transition-colors'
-                style={{width:'100%',height:'100%'}}>
+                key = {`${rowIndex} - ${colIndex}`}
+                className = { `flex items-center justify-center text-lg font-semibold cursor-pointer text-gray-950 bg-white border border-gray-400 ${isSelected ? 'bg-violet-300' : 'hover:bg-violet-200'}  transition-colors`}
+                style={{width:'100%',height:'100%'}}
+                onClick={() => handleClick(rowIndex,colIndex)}>
                     {col}    
-                </div>    
-            ))
+                </div> 
+                )   
+            })
         ))}
     </div>
   )
 }
 
-export default CrossGrid
+export default CrossGrid;
